37dc2950e9cad2ae59ec40109b68d8f91a4133db
aadil42 <adilkelawala@gmail.com>
2026-06-23 12:58:59 +0530

    remove hashing as this is doubly ll not circular. we mistook it for circular

--- count-nodes-in-a-double‑way-linked-list.js
+++ count-nodes-in-a-double‑way-linked-list.js
@@ -1,6 +1,6 @@
 class Solution {
   /**
-   * Time O(n) | Space O(n)
+   * Time O(n) | Space O(1)
    * Counts the number of nodes in a doubly linked list.
    * @param {Object|null} head - The head node of the doubly linked list (or null for empty list).
    * Each node is expected to have the shape `{ val: number, prev: Object|null, next: Object|null }`.
@@ -8,13 +8,10 @@ class Solution {
    */
   countNodes(head) {
     // Your implementation here
-    const hashSet = new Set();
 
     let count = 0;
-      
-    while (head && !hashSet.has(head)) {
-          count += 1;
-        hashSet.add(head);
+    while (head) {
+        count += 1;
         head = head.next;
     }
     return count;
